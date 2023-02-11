b7f66f64d70b3effbce9d8aa15d9bb0170df471e
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2023-02-11 17:08:42 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+ 
+ 
+bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
+bot.remove_command("help") # Borra el comando por defecto !help
+ 
+ 
+@bot.command()
+async def hola(ctx): #Comando a decir
+    await ctx.send('Qué tal estas?') #Mensaje que dira el bot
+ 
+ 
+#Cuándo Ejecutes el comando en discord !hola, el bot dira Qué tal estas?
+ 
+ 
+ 
+@bot.event
+async def on_ready():
+    print("BOT listo!")
+    
+ 
+    
+bot.run('TU TOKEN AQUÍ') #OBTEN UN TOKEN EN: https://discord.com/developers/applications
